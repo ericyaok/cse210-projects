3f03a7f105ec23500f50cf17f97d2724c87dd30d
Eric Kodzi <erickodzi@gmail.com>
2024-04-26 19:17:02 +0100

    Assignemnt Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade in percentage");
+        string getScore = Console.ReadLine();
+        int score = int.Parse(getScore);
+        string letter = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else if (score < 60)
+        {
+            letter = "F";
+        }
+ 
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Hurray! You have passed this course");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, you will need to resit this course");
+        }
     }
 }
