8d146e86caf9cf558594af26285bf61c974f0236
Eric Kodzi <erickodzi@gmail.com>
2024-04-26 23:32:46 +0100

    Prep 5 solution

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Globalization;
+
+
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        Console.WriteLine(DisplayResult(name, square));
+
     }
+
+    static void DisplayWelcome()
+{
+    Console.WriteLine("Welcome To The Program!");
+}
+
+static string PromptUserName()
+{
+    Console.Write("Enter your name: ");
+    string name = Console.ReadLine();
+    return name;
+}
+
+static int PromptUserNumber()
+{
+    Console.Write("Enter your favorite number: ");
+    string getNumber = Console.ReadLine();
+    int number = int.Parse(getNumber);
+    return number;
+}
+
+
+static int SquareNumber(int number)
+{
+    return number * number;
+}
+
+
+static string DisplayResult(string name, int number)
+{
+    return $"Brother {name}, the square of your number is {number}";
+}
+
+
 }
